c3abcb7ae549588a72848b6a32be653a44a9d449
Pedro Oliveira <pedro_oliver13@hotmail.com>
2023-11-10 17:27:57 -0300

    feat: add project in c

---
+++ main.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+enum InitMenuEnum
+{
+    ACCESS_APP = 1,
+};
+
+enum MenuEnum
+{
+    DEPOSIT = 1,
+    WITHDRAWAL = 2,
+    BALANCE = 3,
+    INVESTMENT_CALCULATOR = 4
+};
+
+void main()
+{
+    char userName[98] = "pipoca", password[98] = "1609";
+    char currentUser[98], currentPassword[98];
+    int balance = 0;
+    bool isLogged = false;
+
+    void login()
+    {
+        printf("..:: Login ::..\n");
+        printf("Digite o nome do usuario: ");
+        scanf("%s", &currentUser);
+
+        printf("Digite sua senha: ");
+        scanf("%s", &currentPassword);
+
+        bool isCorrectLogin = strcmp(currentPassword, password) == 0 && strcmp(currentUser, userName) == 0;
+
+        if (isCorrectLogin)
+        {
+            printf("\n>> Logado na conta: %s! <<\n\n", userName);
+            isLogged = true;
+            return;
+        }
+
+        printf("\n>> Login ou senha incorreta! <<\n\n");
+        isLogged = false;
+        login();
+        return;
+    }
+
+    void showBalance()
+    {
+        printf("\nSeu saldo atual e: R$ %d\n\n", balance);
+        return;
+    }
+
+    void calculateDeposit()
+    {
+        int value;
+
+        printf("Quanto voce quer depositar? R$ ");
+        scanf("%d", &value);
+        balance = balance + value;
+        printf("Seu saldo atual ficou: R$ %d\n\n", balance);
+
+        return;
+    }
+
+    void calculateWithdrawal()
+    {
+        int value;
+
+        if (balance == 0)
+        {
+            printf("\n>> Nao tem saldo suficiente! <<\n\n");
+            return;
+        }
+
+        printf("Quanto voce quer sacar? R$ ");
+        scanf("%d", &value);
+
+        value = balance - value;
+
+        if (value < 0)
+        {
+            printf("\n>> Nao tem saldo saldo suficiente! <<\n\n");
+            return;
+        }
+
+        balance = value;
+        printf("Seu saldo atual ficou: R$ %d\n\n", balance);
+    }
+
+    void menu()
+    {
+        int optionSelected, value;
+
+        printf("..:: Menu ::..\n1 - Deposito\n2 - Saque\n3 - Saldo\n4 - Calculadora de investimento\n5 - Sair\n");
+        scanf("%d", &optionSelected);
+
+        switch (optionSelected)
+        {
+        case DEPOSIT:
+            calculateDeposit();
+            menu();
+            break;
+        case WITHDRAWAL:
+            calculateWithdrawal();
+            menu();
+            break;
+        case BALANCE:
+            showBalance();
+            menu();
+            break;
+        case INVESTMENT_CALCULATOR:
+            printf("Selecionou a calculadora de investimento!");
+            break;
+        default:
+            printf("Saiu!");
+        }
+
+        return;
+    }
+
+    void init()
+    {
+        int optionSelected;
+
+        printf("..:: Banco ::..\n\n1 - Acessar sistema\n2 - Sair\n");
+        scanf("%d", &optionSelected);
+
+        if (optionSelected != ACCESS_APP)
+        {
+            printf("Saiu!");
+            return;
+        }
+
+        login();
+
+        if (isLogged)
+        {
+            menu();
+        }
+
+        return;
+    }
+
+    init();
+    return;
+}
